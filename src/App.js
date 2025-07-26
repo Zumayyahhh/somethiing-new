5cadbecbf3ac1e15ea0c2e684e85f2e9a7f2511d
Sumayya Sainu <sumayyasainu2826@gmail.com>
2025-07-26 07:56:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Img2 from './img2.jpeg';
+import Img1 from './asscet/img1.jpeg';
+
+function App() {
+  return (
+     <div className="App">
+      <header className="App-header">
+        <h1>Welcome to ztarzieehh</h1>
+        <img src={Img2} alt="" />
+      <img src={Img1} alt=""/>
+      </header>
+      </div>
+  );
+}
+
+export default App;
